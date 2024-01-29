5bfd181b581b09d59c99a3aea169f90bc7480e3b
daddyJefe <26.jeffrey.noah@Ksd.org>
2024-01-29 10:15:23 -0800

    this is not good

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,7 +14,11 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -22,19 +26,34 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+
 //yo
 BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
 
+private CANSparkMax m_leftMotor;
+private CANSparkMax m_rightMotor;
+private CANSparkMax m_leftMotor2;
+private CANSparkMax m_rightMotor2;
+
+
+
+private RelativeEncoder leftEncoder;
+private RelativeEncoder rightEncoder;
 //no
-
+/** 
   private final PWMSparkMax m_leftDrive = new PWMSparkMax(3);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(4);
+  private final PWMSparkMax m_rightDrive = new PWMSparkMax(2);
   private final DifferentialDrive m_robotDrive =
-
+    new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
   private final PWMSparkMax m_leftDrive2 = new PWMSparkMax(1);
-  private final PWMSparkMax m_rightDrive2 = new PWMSparkMax(2);
+  private final PWMSparkMax m_rightDrive2 = new PWMSparkMax(4);
   private final DifferentialDrive m_robotDrive2 =
-      new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
+      new DifferentialDrive(m_leftDrive2::set, m_rightDrive2::set);
+      */
+
+
+
   private final XboxController m_controller = new XboxController(0);
   private final Timer m_timer = new Timer();
 
@@ -48,8 +67,8 @@ double filteredXAccel = 0.0;
 //delete this if it is tweaking
 
   public Robot() {
-    SendableRegistry.addChild(m_robotDrive, m_leftDrive);
-    SendableRegistry.addChild(m_robotDrive, m_rightDrive);
+   // SendableRegistry.addChild(m_robotDrive, m_leftDrive);
+   // SendableRegistry.addChild(m_robotDrive, m_rightDrive);
   }
 
   /**
@@ -58,10 +77,31 @@ double filteredXAccel = 0.0;
    */
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightDrive.setInverted(true);
+     /**
+   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+   * 
+   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+   * first parameter
+   * 
+   * The motor type is passed as the second parameter. Motor type can either be:
+   *  com.revrobotics.CANSparkLowLevel.MotorType.kBrushless
+   *  com.revrobotics.CANSparkLowLevel.MotorType.kBrushed
+   * 
+   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
+   * these parameters to match your setup
+   */
+  m_leftMotor = new CANSparkMax(4, MotorType.kBrushless);
+  m_rightMotor = new CANSparkMax(2, MotorType.kBrushless);
+
+  /**
+   * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+   * in the SPARK MAX to their factory default state. If no argument is passed, these
+   * parameters will not persist between power cycles
+   */
+  m_leftMotor.restoreFactoryDefaults();
+  m_rightMotor.restoreFactoryDefaults();
+
+  m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
   }
 
   /** This function is run once each time the robot enters autonomous mode. */
@@ -73,16 +113,11 @@ double filteredXAccel = 0.0;
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
-      // Drive forwards half speed, make sure to turn input squaring off
-      m_robotDrive.arcadeDrive(0.5, 0.0, false);
-    } else {
-      m_robotDrive.stopMotor(); // stop robot
+    {
     }
 
-   
   }
+  
 
   /** This function is called once each time the robot enters teleoperated mode. */
   @Override
@@ -94,16 +129,23 @@ double filteredXAccel = 0.0;
   @Override
   public void teleopPeriodic() {
  
-    double filteredXAccel = xAccelFilter.calculate(accelerometer.getX());
-    SmartDashboard.putNumber(  "IMU_Pitch", filteredXAccel);
+   // SmartDashboard.putNumber("Left Encoder", leftEncoder.getPosition());
+//SmartDashboard.putNumber("Right Encoder", rightEncoder.getPosition());
 
-    System.out.println("X pressed? "+(m_controller.getXButtonPressed()));
+
+    //frontDrive.arcadeDrive(-m_controller.getLeftY(), m_controller.getRightX());
+   // rearDrive.arcadeDrive(-m_controller.getLeftY(), m_controller.getRightX());
     
-    m_robotDrive.arcadeDrive(-m_controller.getLeftY(), m_controller.getRightX());
-    m_robotDrive2.arcadeDrive(-m_controller.getLeftY(), m_controller.getRightX());
-    
+   m_myRobot.arcadeDrive(-1, 0);
    
   }
+
+  @Override
+  public void disabledPeriodic(){}
+
+  @Override
+  public void robotPeriodic(){}
+
   /** This function is called once each time the robot enters test mode. */
   @Override
   public void testInit() {}
