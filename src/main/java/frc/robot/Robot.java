e50abf744a71177fc913be98142d1ccc6c004cb9
daddyJefe <26.jeffrey.noah@Ksd.org>
2024-01-29 14:35:31 -0800

    the code works now!!!!! only 2 brushless motortho

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+ //private final XboxController m_controller = new XboxController(0);  
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
@@ -27,57 +28,14 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
  */
 public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
+ 
 
-//yo
-BuiltInAccelerometer accelerometer = new BuiltInAccelerometer();
-
-private CANSparkMax m_leftMotor;
-private CANSparkMax m_rightMotor;
-private CANSparkMax m_leftMotor2;
-private CANSparkMax m_rightMotor2;
-
-
-
-private RelativeEncoder leftEncoder;
-private RelativeEncoder rightEncoder;
-//no
-/** 
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(3);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(2);
-  private final DifferentialDrive m_robotDrive =
-    new DifferentialDrive(m_leftDrive::set, m_rightDrive::set);
-  private final PWMSparkMax m_leftDrive2 = new PWMSparkMax(1);
-  private final PWMSparkMax m_rightDrive2 = new PWMSparkMax(4);
-  private final DifferentialDrive m_robotDrive2 =
-      new DifferentialDrive(m_leftDrive2::set, m_rightDrive2::set);
-      */
-
-
-
-  private final XboxController m_controller = new XboxController(0);
-  private final Timer m_timer = new Timer();
-
-//stupid code goes here
-double prevXAccel = 0.0;
-double prevYAccel = 0.0;
-// navX MXP using SPI
-LinearFilter xAccelFilter = LinearFilter.movingAverage(10);
-double filteredXAccel = 0.0;
-
-//delete this if it is tweaking
-
-  public Robot() {
-   // SendableRegistry.addChild(m_robotDrive, m_leftDrive);
-   // SendableRegistry.addChild(m_robotDrive, m_rightDrive);
-  }
-
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private CANSparkMax m_leftMotor;
+  private CANSparkMax m_rightMotor;
+private final XboxController m_controller = new XboxController(0);  
   @Override
   public void robotInit() {
-     /**
+  /**
    * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
    * 
    * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
@@ -90,69 +48,35 @@ double filteredXAccel = 0.0;
    * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
    * these parameters to match your setup
    */
-  m_leftMotor = new CANSparkMax(4, MotorType.kBrushless);
-  m_rightMotor = new CANSparkMax(2, MotorType.kBrushless);
+    m_leftMotor = new CANSparkMax(3, MotorType.kBrushless);
+    m_rightMotor = new CANSparkMax(2, MotorType.kBrushless);
 
-  /**
-   * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-   * in the SPARK MAX to their factory default state. If no argument is passed, these
-   * parameters will not persist between power cycles
-   */
-  m_leftMotor.restoreFactoryDefaults();
-  m_rightMotor.restoreFactoryDefaults();
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+//m_leftMotor.setInverted(true);
+    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
 
-  m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-  }
-
-  /** This function is run once each time the robot enters autonomous mode. */
-  @Override
-  public void autonomousInit() {
-    m_timer.restart();
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    {
-    }
-
-  }
-  
-
-  /** This function is called once each time the robot enters teleoperated mode. */
-  @Override
-  public void teleopInit() {
     
   }
 
-  /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
- 
-   // SmartDashboard.putNumber("Left Encoder", leftEncoder.getPosition());
-//SmartDashboard.putNumber("Right Encoder", rightEncoder.getPosition());
+    m_myRobot.arcadeDrive(m_controller.getLeftY(), m_controller.getRightX());
 
-
-    //frontDrive.arcadeDrive(-m_controller.getLeftY(), m_controller.getRightX());
-   // rearDrive.arcadeDrive(-m_controller.getLeftY(), m_controller.getRightX());
-    
-   m_myRobot.arcadeDrive(-1, 0);
-   
   }
 
-  @Override
-  public void disabledPeriodic(){}
 
   @Override
-  public void robotPeriodic(){}
+public void disabledPeriodic() {
 
-  /** This function is called once each time the robot enters test mode. */
-  @Override
-  public void testInit() {}
+}
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
-
+@Override
+public void robotPeriodic() {
   
 }
+}
+
