7daed17faf313253e3b8f8fb410e63298fc2bd60
daddyJefe <26.jeffrey.noah@ksd.org>
2024-03-05 16:09:31 -0800

    this is so cool

---
+++ src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,133 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+/** Represents a differential drive style drivetrain. */
+public class Drivetrain {
+
+  public static final double kMaxSpeed = 3.0; // meters per second
+  public static final double kMaxAngularSpeed = 2 * Math.PI; // one rotation per second
+
+  private static final double kTrackWidth = 0.381 * 2; // meters
+  private static final double kWheelRadius = 0.0508; // meters
+  private static final int kEncoderResolution = 4096;
+
+  private final CANSparkMax m_leftLeader = new CANSparkMax(1, MotorType.kBrushless);
+    private final CANSparkMax m_leftFollower = new CANSparkMax(2, MotorType.kBrushless);
+      private final CANSparkMax m_rightLeader = new CANSparkMax(4, MotorType.kBrushless);
+        private final CANSparkMax m_rightFollower = new CANSparkMax(3, MotorType.kBrushless);
+  
+  DifferentialDrive bobotDrive = new DifferentialDrive(m_leftLeader, m_rightLeader);
+  
+
+  RelativeEncoder m_leftEncoder = m_leftLeader.getEncoder();
+  RelativeEncoder m_rightEncoder = m_rightLeader.getEncoder();
+  //AHRS ahrs = new AHRS(SPI.Port.kMXP);
+  AHRS ahrs = new AHRS(SPI.Port.kMXP);
+  //AnalogGyro AnalogGyro = ahrs.getRotation2d();
+  
+  private final PIDController m_leftPIDController = new PIDController(1, 0, 0);
+  private final PIDController m_rightPIDController = new PIDController(1, 0, 0);
+
+  private final DifferentialDriveKinematics m_kinematics =
+      new DifferentialDriveKinematics(kTrackWidth);
+
+  private final DifferentialDriveOdometry m_odometry;
+
+  // Gains are for example purposes only - must be determined for your own robot!
+  private final SimpleMotorFeedforward m_feedforward = new SimpleMotorFeedforward(1, 3);
+
+  /**
+   * Constructs a differential drive object. Sets the encoder distance per pulse and resets the
+   * gyro.
+   */
+  public Drivetrain() {
+    ahrs.zeroYaw();
+
+    m_leftFollower.follow(m_leftLeader);
+    m_rightFollower.follow(m_rightLeader);
+
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+  
+
+    // Set the distance per pulse for the drive encoders. We can simply use the
+    // distance traveled for one rotation of the wheel divided by the encoder
+    // resolution.
+ 
+    m_odometry =
+        new DifferentialDriveOdometry(
+            ahrs.getRotation2d(), m_leftEncoder.getPosition(), m_rightEncoder.getPosition(), 
+            new Pose2d(5.0, 13.5, new Rotation2d()));
+  }
+
+  /**
+   * Sets the desired wheel speeds.
+   *
+   * @param speeds The desired wheel speeds.
+   */
+  public void setSpeeds(DifferentialDriveWheelSpeeds speeds) {
+    final double leftFeedforward = m_feedforward.calculate(speeds.leftMetersPerSecond);
+    final double rightFeedforward = m_feedforward.calculate(speeds.rightMetersPerSecond);
+
+    final double leftOutput =
+        m_leftPIDController.calculate(m_leftEncoder.getVelocity(), speeds.leftMetersPerSecond);
+    final double rightOutput =
+        m_rightPIDController.calculate(m_rightEncoder.getVelocity(), speeds.rightMetersPerSecond);
+    //m_leftLeader.set(leftOutput + leftFeedforward);
+    //m_rightLeader.set(rightOutput + rightFeedforward);
+
+
+  }
+
+  public void doArcadeDrive(double controllerX, double controllerY){
+
+        bobotDrive.arcadeDrive(controllerX , controllerY);
+
+  }
+
+  /**
+   * Drives the robot with the given linear velocity and angular velocity.
+   *
+   * @param xSpeed Linear velocity in m/s.
+   * @param rot Angular velocity in rad/s.
+   */
+  public void drive(double xSpeed, double rot) {
+    var wheelSpeeds = m_kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
+    setSpeeds(wheelSpeeds);
+  }
+
+  /** Updates the field-relative position. */
+  public void updateOdometry() {
+    m_odometry.update(
+        ahrs.getRotation2d(), m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
+  }
+
+  public void printOdemetry(){
+    System.out.println(m_odometry.getPoseMeters());
+  }
+
+  
+
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,280 +1,60 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.RobotController;
-import java.nio.file.Path;
-import java.util.concurrent.TimeUnit;
-import edu.wpi.first.math.filter.LinearFilter;
-import edu.wpi.first.math.trajectory.TrajectoryUtil;
-import edu.wpi.first.util.sendable.SendableRegistry;
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.AnalogInput;
-import edu.wpi.first.wpilibj.DigitalOutput;
- //private final XboxController m_controller = new XboxController(0);  
-import com.revrobotics.CANSparkMax;
 
-import java.util.concurrent.TimeUnit;
 
 import com.kauailabs.navx.frc.AHRS;
-import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the manifest file in the resource
- * directory.
- */
-
-
-
-  /**
-   * Imports a Trajectory from a JSON file exported from PathWeaver.
-   *
-   * @param path The path of the json file to import from
-   * @return The trajectory represented by the file.
-   * @throws IOException if reading from the file fails.
-   */
-
- 
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
-           
+  final AHRS ahrs = new AHRS(SPI.Port.kMXP);
+  private final XboxController m_controller = new XboxController(0);
+  private final Drivetrain m_drive = new Drivetrain();
 
-  public DigitalOutput ultrasonicTriggerPinOne = new DigitalOutput(0);
-  public AnalogInput ultrasonicSensorOne = new AnalogInput(0);
+  // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
+  private final SlewRateLimiter m_speedLimiter = new SlewRateLimiter(3);
+  private final SlewRateLimiter m_rotLimiter = new SlewRateLimiter(3);
 
-  public double ultrasonicSensorOneRange = 0;
-  public double voltageScaleFactor = 1;
 
-  public void turnOnSensorOne(){
-    ultrasonicTriggerPinOne.set(true);
+  @Override 
+  public void robotInit(){
+    
   }
-
-           AHRS ahrs = new AHRS(SPI.Port.kMXP);
-       
-
-   
-  private DifferentialDrive m_myRobot;
-  //secondary differentialDrive created to drive the rear wheel systems
-  private DifferentialDrive m_rearRobot;
-private Command autonomousCommand;
-  private CANSparkMax m_leftMotor;
-  private CANSparkMax m_rightMotor;
-  int firing = 0;
-  int loading = 0;
-  private CANSparkMax m_leftRearMotor;
-  private CANSparkMax m_rightRearMotor;
-
-  //declaring motor encoders for data feedback!
-  private RelativeEncoder leftEncoder;
-  private RelativeEncoder rightEncoder;
-
-  private CANSparkMax feedWheel;
-  private CANSparkMax launchWheel;
-  private final Timer m_Timer = new Timer();
-
-private final XboxController m_controller = new XboxController(0);  
-
-//Autonomous Mode Code
-private static final String testAuto ="testAuto";
-private static final String testAuto2 = "testAuto2";
-private String m_AutoSelected;
-private final SendableChooser<String> m_Chooser = new SendableChooser<>();
-private int m_count;
-
-
   @Override
-  public void robotInit() {
-
-    SmartDashboard.putNumber("Sensor 1 Range", 500);
-
-    CameraServer.addServer("cam 1");
-     CameraServer.addServer("cam 2");
-    UsbCamera camera = CameraServer.startAutomaticCapture();
-   
-              // Set the resolution
-              camera.setResolution(160, 120);
-
-     UsbCamera camera2 = CameraServer.startAutomaticCapture();
-
-              camera2.setResolution(160, 120);
-  /**
-   * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-   * 
-   * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-   * first parameter
-   * 
-   * The motor type is passed as the second parameter. Motor type can either be:
-   *  com.revrobotics.CANSparkLowLevel.MotorType.kBrushless
-   *  com.revrobotics.CANSparkLowLevel.MotorType.kBrushed
-   * 
-   * The example below initializes four brushless motors with CAN IDs 1 and 2. Change
-   * these parameters to match your setup
-   */
-    m_leftMotor = new CANSparkMax(1, MotorType.kBrushless);
-    m_rightMotor = new CANSparkMax(4, MotorType.kBrushless);
-     m_leftRearMotor = new CANSparkMax(2, MotorType.kBrushless);
-    m_rightRearMotor = new CANSparkMax(3, MotorType.kBrushless);
-
-    feedWheel = new CANSparkMax(5, MotorType.kBrushless);
-    launchWheel = new CANSparkMax(6, MotorType.kBrushless);
-    /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
-     */
-//m_leftMotor.setInverted(true);
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-    m_rearRobot = new DifferentialDrive(m_leftRearMotor, m_rightRearMotor);
-    //this code sets up the encoders with their respective motors
-    leftEncoder = m_leftRearMotor.getEncoder();
-    rightEncoder = m_rightRearMotor.getEncoder();
-
-    //Autonomous Mode Code
-    m_Chooser.setDefaultOption("Default Test Auto", testAuto);
-    m_Chooser.addOption("Option 2 Test", testAuto2);
-    SmartDashboard.putData("Auto Choices", m_Chooser);
-    
-    
+  public void autonomousPeriodic() {
+    teleopPeriodic();
+    m_drive.updateOdometry();
   }
 
   @Override
   public void teleopPeriodic() {
     
-//do camera streaming
-//CameraServer.startAutomaticCapture();
+    m_drive.doArcadeDrive(m_controller.getLeftY(), m_controller.getRightX());
+    // Get the x speed. We are inverting this because Xbox controllers return
+    // negative values when we push forward.
+    final var xSpeed = -m_speedLimiter.calculate(m_controller.getLeftY()) * Drivetrain.kMaxSpeed;
+    
+    // Get the rate of angular rotation. We are inverting this because we want a
+    // positive value when we pull to the left (remember, CCW is positive in
+    // mathematics). Xbox controllers return positive values when you pull to
+    // the right by default.
+    final var rot = -m_rotLimiter.calculate(m_controller.getRightX()) * Drivetrain.kMaxAngularSpeed;
 
-
-
-
-
-  m_myRobot.arcadeDrive(m_controller.getLeftY()*.85,m_controller.getRightX()*.5);
-  m_rearRobot.arcadeDrive(m_controller.getLeftY()*.85, m_controller.getRightX()*.5);
-
-  //this prints the positions of the motors in realtime
-  SmartDashboard.putNumber("Left Encoder", leftEncoder.getPosition());
-  SmartDashboard.putNumber("Right Encoder", rightEncoder.getPosition());
-  SmartDashboard.putBoolean(  "IMU_Connected",        ahrs.isConnected());
-  SmartDashboard.putBoolean(  "IMU_IsCalibrating",    ahrs.isCalibrating());
-  SmartDashboard.putNumber(   "IMU_Yaw",              ahrs.getYaw());
-  SmartDashboard.putNumber(   "IMU_Pitch",            ahrs.getPitch());
-  SmartDashboard.putNumber(   "IMU_Roll",             ahrs.getRoll());
-
-    if(m_controller.getRightTriggerAxis() > 0.1){
-
-     
-
-
-      if(firing == 0){
-        launchWheel.set(1);
-        Timer.delay(.4);
-         feedWheel.set(1);
-         firing = 1;
-         Timer.delay(1);
-         launchWheel.set(0);
-         feedWheel.set(0);
-         firing = 0;
-     
-    }
-  }
-    if(m_controller.getLeftTriggerAxis() > 0.1){
-
-     
-
-
-      if(loading == 0){
-        launchWheel.set(-.2);
-         feedWheel.set(-.2);
-         firing = 0;
-         loading = 1;
-        Timer.delay(.4);
-     launchWheel.set(0);
-     feedWheel.set(0);
-        loading = 0;
-      }
-    }
-  
-    /** 
-    if(m_Timer.get() < 2.0){
-      launchWheel.set(0.9);
-    }else if(m_Timer.get() < 4.0){
-      launchWheel.set(0.9);
-      feedWheel.set(0.9);
-    }else{
-      launchWheel.set(0.0);
-      feedWheel.set(0.0);
-    }
-*/
-
+    m_drive.drive(xSpeed, rot);
+    
+m_drive.updateOdometry();
+m_drive.printOdemetry();
   }
 
+  public void robotPeriodic(){
 
-  @Override
-public void disabledPeriodic() {
 
-}
 
-@Override
-public void robotPeriodic() {
-   ultrasonicSensorOneRange = (ultrasonicSensorOne.getValue()*voltageScaleFactor*0.125);
-  SmartDashboard.putNumber("Sensor 1 Range", ultrasonicSensorOneRange);
-  voltageScaleFactor = 5/RobotController.getVoltage5V();
-}
-
-@Override
-public void autonomousInit() {
-m_AutoSelected = m_Chooser.getSelected();
-m_count = 0;
-}
-
-@Override
-public void autonomousPeriodic(){
-  
-m_leftRearMotor.set(.5);
-m_rightRearMotor.set(.5);
-m_leftMotor.set(.5);
-m_rightMotor.set(.5);
-// Timer.delay(3);
-
-if(m_count > 100){
-m_leftRearMotor.set(0);
-m_rightRearMotor.set(0);
-m_leftMotor.set(0);
-m_rightMotor.set(0);
-}
-m_count++;
-
-  switch(m_AutoSelected){
-    case testAuto2:
-    //custom code goes here
-    case testAuto:
-    default:
-    //Put default code here
-    break;
+    
   }
-
-
 }
-
-}
-
